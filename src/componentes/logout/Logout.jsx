363eb924224b053f1a634d82d5dc9f1f9d7acd31
Sofia <sofiademello372@gmail.com>
2024-10-07 21:05:45 -0300

    Incluido pagina Logout 07/10

---
+++ src/componentes/logout/Logout.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "./Logout.css";
+
+
+function Logout() {
+  const [formData, setFormData] = useState({
+
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // chamada para o backend
+    console.log(formData);
+  };
+
+  return (
+    <div class="Logout-container">
+      <div className="form-background">
+        <form onSubmit={handleSubmit}>
+          <div className="Logout-title">
+            <h1>Deseja mesmo encerrar sessão?</h1>
+          </div>
+          <div class="button-container">
+            <button className="button1">Sim</button>
+            <button className="button2">Não</button>
+          </div>
+          <div className="Plano-de-Fundo"></div>
+        </form>
+      </div>
+    </div>
+
+  );
+
+}
+
+export default Logout;
