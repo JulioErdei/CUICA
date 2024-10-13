b5176aea9d32de9a783ff7f755531aa5e3f4afa7
Rodrigo-04 <rodrigomgm03@gmail.com>
2024-10-13 10:00:05 -0300

    Atulização na tela de créditos 13/10

--- src/componentes/credito/credito.jsx
+++ src/componentes/credito/credito.jsx
@@ -23,19 +23,42 @@ function Credito() {
         <div className="quadro">
             <div className='quadro-container'>
               <div className="title">Créditos</div>
-                <h2>Integrantes</h2>
-                <p>Diogo</p>
-                <p>Gabriel</p>
-                <p>Guilherme</p>
-                <p>Isabela</p>
-                <p>Julio</p>
-                <p>Pedro</p>
-                <p>Renato</p>
-                <p>Rodrigo</p>
-                <p>Sofia</p>
-                <p>Tamires</p>
-                <h2>Agradecimentos</h2>
-                <p>Professor Victor</p>
+                <div className='subtitle'>
+                  <p>Este jogo foi realizado como parte do curso de Análise e
+                  Desenvolvimento de Tecnologia na FATEC São Paulo.</p>
+                </div>
+                <div className='texto'>
+                  {/* <dl>
+                    <dt>Desenvolvimento Jogo e fucionalidades:</dt>
+                    <dd>Renato Caetité</dd>
+                    <dd>Guilherme Santo</dd>
+                    <dd>Isabela Ramos</dd>
+                    <dt>Desenvolvimento site:</dt>
+                    <dd>Julio Cezar Erdei</dd>
+                    <dd>Sofia de Mello</dd>
+                    <dd>Tamires Barboza</dd>
+                    <dd>Rodrigo Marcato </dd>
+                    <dd>Pedro Henrique</dd>
+                    <dd>Diogo Souza </dd>
+                    <dt>Arte e Design: </dt>
+                    <dd>Isabela Ramos</dd>
+                  </dl> */}
+                  <ul>
+                    <li className='list-title'>Desenvolvimento Jogo e fucionalidades:</li>
+                    <li className='list-text'>Renato Caetité</li>
+                    <li className='list-text'>Guilherme Santo</li>
+                    <li className='list-text'>Isabela Ramos</li>
+                    <li className='list-title'>Desenvolvimento site:</li>
+                    <li className='list-text'>Julio Cezar Erdei</li>
+                    <li className='list-text'>Sofia de Mello</li>
+                    <li className='list-text'>Tamires Barboza</li>
+                    <li className='list-text'>Rodrigo Marcato </li>
+                    <li className='list-text'>Pedro Henrique</li>
+                    <li className='list-text'>Diogo Souza </li>
+                    <li className='list-title'>Arte e Design: </li>
+                    <li className='list-text'>Isabela Ramos</li>
+                  </ul>
+                </div>
               <button type="submit">Fechar</button>
             </div>
         </div>
