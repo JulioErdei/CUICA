ea94b42a65756c272096d29a1f349243b55d25ea
Julio Erdei <juliocezaredei1234@gmail.com>
2024-10-20 13:29:15 -0300

    Alteração tela Loja

--- src/componentes/loja/moedas/LojaMoeda.jsx
+++ src/componentes/loja/moedas/LojaMoeda.jsx
@@ -6,6 +6,8 @@ import ReturnIcon from '/public/assets/imagens/icones/ReturnIcon';
 import ModalCompraMoeda from '/src/componentes/modals/compras/moedas/compraMoedas.jsx';
 import useAuthConta from '/src/hooks/AuthConta';
 import LojaMoedaService from '/src/services/LojaMoedasService';
+import gameIcon from './img/icon2.png';
+import soundIcon from './img/soundicon.png';
 
 function LojaMoeda() {
     const navigate = useNavigate()
@@ -58,85 +60,58 @@ function LojaMoeda() {
         };
 
         compraMoedas(user.jogador.id, tipoMoeda, qtdeMoeda);
+
     }
     
     return(
         <section className="bg-loja-moeda">
             <div className="bg-loja-moeda-container" id="bg-lojaMoeda">
-                <div className="menu-superior-loja-moeda">
-                    <div className="menu-loja-moeda">
-                        <button className="item-retorno-loja-moeda" onClick={() => {navigate("/menu")}}>
-                            <ReturnIcon />
-                        </button>
+                <header className="menu-loja">                  
+                    <nav className="menu-options-loja">
+                        <a onClick={() => {navigate("/menu")}}>
+                            <img src={gameIcon} alt="Jogo da Onça" className="game-logo-loja" />
+                        </a>
+                        <a onClick={() => {navigate("/tutorial")}}>Regras</a>
+                        <a onClick={() => {navigate("/credito")}}>Créditos</a>
+                        <a onClick={() => {navigate("/Logout")}}>Log Out</a>
+                    </nav>
+                </header>
+                <div className="form-background-loja">
+                    <div className="moedas-atuais-loja-moeda">
+                        <div className="item-qtde-loja-moeda moedas-loja-moeda">
+                            <div className="icon-loja-moeda icon-moeda-loja-moeda"></div>
+                            <div className="info-esmeralda-loja-moeda">
+                                <p className="info-p-loja-moeda">{user?.jogador?.qntmoedanormal}</p>
+                            </div>
+                        </div>
+                        <div className="loja-title">Loja</div>
                     </div>
-                    <div className="titulo-loja-moeda">
-                        <div className="placa-titulo-loja-moeda">
-                            <h1>LOJA</h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="moedas-atuais-loja-moeda">
-                    <div className="item-qtde-loja-moeda esmeraldas-loja-moeda">
-                        <div className="icon-loja-moeda icon-esmeralda-loja-moeda"></div>
-                        <div className="info-esmeralda-loja-moeda">
-                            <p className="info-p-loja-moeda">{user?.jogador?.qntmoedaespecial}</p>
-                        </div>
-                    </div>
-                    <div className="item-qtde-loja-moeda moedas-loja-moeda">
-                        <div className="icon-loja-moeda icon-moeda-loja-moeda"></div>
-                        <div className="info-esmeralda-loja-moeda">
-                            <p className="info-p-loja-moeda">{user?.jogador?.qntmoedanormal}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="loja-loja-moeda">
-                    <div className="loja-esmeralda-loja-moeda">
-                        <div className="loja-esmeralda-item-loja-moeda">
-                            <div className="foto-item-loja-moeda esmeralda-p-loja-moeda"></div>
-                            <div className="nome-item-loja-moeda">
-                                <h1>100 ESMERALDAS</h1>
+                    <div className="loja-loja-moeda">
+                        <div className="loja-moeda-loja-moeda">
+                            <div className="loja-moeda-item-loja-moeda">
+                                <div className="foto-item-loja-moeda moeda-p-loja-moeda">
+                                </div>
+                                <div className="nome-item-loja-moeda">
+                                    <h1>100 MOEDAS</h1>
+                                </div>
+                                <button className="valor-loja-moeda" value={4} onClick={compraMoeda}>R$5,00</button>
                             </div>
-                            <button className="valor-loja-moeda" value="1" id="1" onClick={compraMoeda}>R$10,00</button>
-                        </div>
-                        <div className="loja-esmeralda-item-loja-moeda">
-                            <div className="foto-item-loja-moeda esmeralda-m-loja-moeda"></div>
-                            <div className="nome-item-loja-moeda">
-                                <h1>500 ESMERALDAS</h1>
+                            <div className="loja-moeda-item-loja-moeda">
+                                <div className="foto-item-loja-moeda moeda-m-loja-moeda">
+                                </div>
+                                <div className="nome-item-loja-moeda">
+                                    <h1>1.000 MOEDAS</h1>
+                                </div>
+                                <button className="valor-loja-moeda" value={5} onClick={compraMoeda}>R$20,00</button>
                             </div>
-                            <button className="valor-loja-moeda" value="2" onClick={compraMoeda}>R$50,00</button>
-                        </div>
-                        <div className="loja-esmeralda-item-loja-moeda">
-                            <div className="foto-item-loja-moeda esmeralda-g-loja-moeda"></div>
-                            <div className="nome-item-loja-moeda">
-                                <h1>1.000 ESMERALDAS</h1>
+                            <div className="loja-moeda-item-loja-moeda">
+                                <div className="foto-item-loja-moeda moeda-g-loja-moeda">
+                                </div>
+                                <div className="nome-item-loja-moeda">
+                                    <h1>10.000 MOEDAS</h1>
+                                </div>
+                                <button className="valor-loja-moeda" value={6} onClick={compraMoeda}>R$50,00</button>
                             </div>
-                            <button className="valor-loja-moeda" value={3} onClick={compraMoeda}>R$100,00</button>
-                        </div>
-                    </div>
-                    <div className="loja-moeda-loja-moeda">
-                        <div className="loja-moeda-item-loja-moeda">
-                            <div className="foto-item-loja-moeda moeda-p-loja-moeda">
-                            </div>
-                            <div className="nome-item-loja-moeda">
-                                <h1>100 MOEDAS</h1>
-                            </div>
-                            <button className="valor-loja-moeda" value={4} onClick={compraMoeda}>R$5,00</button>
-                        </div>
-                        <div className="loja-moeda-item-loja-moeda">
-                            <div className="foto-item-loja-moeda moeda-m-loja-moeda">
-                            </div>
-                            <div className="nome-item-loja-moeda">
-                                <h1>1.000 MOEDAS</h1>
-                            </div>
-                            <button className="valor-loja-moeda" value={5} onClick={compraMoeda}>R$20,00</button>
-                        </div>
-                        <div className="loja-moeda-item-loja-moeda">
-                            <div className="foto-item-loja-moeda moeda-g-loja-moeda">
-                            </div>
-                            <div className="nome-item-loja-moeda">
-                                <h1>10.000 MOEDAS</h1>
-                            </div>
-                            <button className="valor-loja-moeda" value={6} onClick={compraMoeda}>R$50,00</button>
                         </div>
                     </div>
                 </div>
