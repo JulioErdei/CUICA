1a095b6c08b4181e27bc197449a673d248270c73
Tamires <tamires.brbz00@gmail.com>
2024-11-16 16:50:10 -0300

    ACABA PELO AMOR DE DEEEEUSgit add .!

--- src/componentes/header/header.jsx
+++ src/componentes/header/header.jsx
@@ -10,7 +10,7 @@ import backgroundMusic from './sons/ambiente2.wav';
 const Header = () => {
   const navigate = useNavigate();
   const { user } = useUser();
-  const [isSoundOn, setIsSoundOn] = useState(false);
+  const [isSoundOn, setIsSoundOn] = useState(true);
   const audioRef = useRef(null);
 
   // Controle de áudio
@@ -45,7 +45,7 @@ const Header = () => {
 
   return (
     <header className="menu-cadastro">
-        {/* Exibindo o nome do usuário no canto direito */}
+      <nav className="menu-options-cadastro">
         {userLocal ? (
           <>
             <Link to="/menuLogado">
@@ -79,65 +79,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
-
-
-// import React, {useState, useRef, useEffect} from 'react';
-// import './header.css'
-// import { useNavigate } from 'react-router-dom';
-// import { useUser } from '../../axios/userContext'; // Importa o hook do contexto
-// import gameIcon from './images/icon2.png'; // Importe seu ícone de jogo (ajuste o caminho conforme necessário)
-// import soundIcon from './images/soundicon.png'; // Importe o ícone de som (ajuste o caminho conforme necessário)
-// import soundOffIcon from './images/icon-sound-off.png'
-// import backgroundMusic from './sons/ambiente2.wav';
-
-// const Header = () => {
-//   const navigate = useNavigate(); // Função de navegação
-//   const { user } = useUser(); // Acesso ao usuário do contexto
-
-//   const [isSoundOn, setIsSoundOn] = useState(true);
-//   const audioRef = useRef(new Audio(backgroundMusic)); // Refs para controlar o áudio
-
-//   // Função para alternar o som (exemplo)
-//   const toggleSound = () => {
-//     if (isSoundOn) {
-//       audioRef.current.pause(); // Pausa a música
-//     } else {
-//       audioRef.current.play(); // Começa a música
-//     }
-//     setIsSoundOn(!isSoundOn);
-//   };
-
-//   //dividir header para quem esta logado e quem não está =============================
-//   return (
-//     <header className="menu-cadastro">
-//       <nav className="menu-options-cadastro">
-//         <a onClick={() => {localStorage.clear()}}>limpa</a>
-//         <a onClick={() => { navigate("/menu"); }}>
-//           <img src={gameIcon} alt="Jogo da Onça" className="game-logo-cadastro" />
-//         </a>
-//         <a onClick={() => { navigate("/cadastro"); }}>Cadastro</a>
-//         <a onClick={() => { navigate("/login"); }}>Login</a>
-//         <a onClick={() => { navigate("/tutorial"); }}>Regras</a>
-//         <a onClick={() => { navigate("/credito"); }}>Créditos</a>
-//         <a onClick={() => { navigate("/loja/moedas"); }}>Loja</a>
-//         <a onClick={() => {navigate("/Logout")}}>Logout</a>
-
-//         {/* Exibindo o nome do usuário no canto direito */}
-//         {user && (
-//           <div className="user-info-cadastro" onClick={() => navigate("/perfil")}>
-//             <p>{user.username}</p> {/* Nome do usuário, clicável */}
-//           </div>
-//         )}
-
-//         <a onClick={toggleSound}>
-//           <img src={isSoundOn ? soundIcon : soundOffIcon} alt="Som do Jogo" className="sound-icon-cadastro" />
-//         </a>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default Header;
