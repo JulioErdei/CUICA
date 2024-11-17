fa18c1284dc3779479077a3497648ea57a8f4b67
Tamires <tamires.brbz00@gmail.com>
2024-11-17 12:14:53 -0300

    segura na mão de Deus

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: 'https://jogo-on-a-production.up.railway.app/',
     // baseURL: ""
     headers: {
         "Content-Type": "application/json",
