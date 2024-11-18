c84406d772ba5cd5ee9e174f759326727ff72a2c
Rodrigo-04 <rodrigomgm03@gmail.com>
2024-11-18 20:57:22 -0300

    atualizando moeda quando a navegação de loja for usada 18/11/2024

--- src/axios/config.js
+++ src/axios/config.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: 'https://jogo-on-a-production.up.railway.app/',
-    // baseURL: ""
     headers: {
         "Content-Type": "application/json",
     }
