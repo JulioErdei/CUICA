eab188824dadbc55cabc5c4e0112fd3db77dcdd2
Tamires <tamires.brbz00@gmail.com>
2024-11-19 08:11:25 -0300

    vai ficar mais leve sim

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,7 @@ import HomeLog from './componentes/homeLog/HomeLog.jsx';
 import Login from './componentes/login/Login2.jsx';
 import Novasenha from './componentes/Novasenha/novasenha.jsx';
 import LojaMoeda from './componentes/loja/moedas/LojaMoeda.jsx';
-import LojaSkin from './componentes/loja/skins/LojaSkin.jsx';
-import CompraMoedas from './componentes/modals/compras/moedas/compraMoedas.jsx';
-import CompraSkins from './componentes/modals/compras/skins/compraSkin.jsx';
-import Derrota from './componentes/modals/derrota/derrota.jsx';
-import Desistencia from './componentes/modals/derrota/desistencia/desistencia.jsx';
-import Desistir from './componentes/modals/desistir/desistir.jsx';
-import Erro from './componentes/modals/erro/erro.jsx';
-import Fila from './componentes/modals/fila/fila.jsx';
-import Sucesso from './componentes/modals/sucesso/sucesso.jsx';
 import Tutorial from './componentes/regras/regras.jsx';
-import Vitoria from './componentes/modals/vitoria/vitoria.jsx';
 import Tabuleiro from './componentes/tabuleiro/Tabuleiro.jsx';
 import GlobalProvider from './providers/GlobalProvider.jsx';
 import Cadastro from './componentes/Cadastro/Cadastro.jsx';
@@ -29,8 +19,6 @@ import { UserProvider, useUser } from './axios/userContext.jsx';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 
 const App = () => {
-  // const { signed } = useAuthConta();
-  // const {user} = useUser(user) || {};
   
   const username = localStorage.getItem('username');
   var logged = false;
@@ -49,12 +37,6 @@ const App = () => {
         <BrowserRouter>
         <GlobalProvider>
           <Routes>
-              {/* <Route path='/' element={signed ? <Navigate to={"/menu"}/> : <Navigate to={"/menu"}/>}/> */}
-              {/* {user ? (<Route path='/menu' element={<Home />} />):(<Route path='/menuLogado' element={<HomeLog />} />)} */}
-              {/* <Route path='/' element={user ? (<Navigate to={'/menu'} />):(<Navigate to ={'/menuLogado'} />)} /> */}
-              {/* <Route path='/' element={ Logged ? <Navigate to={'/menuLogado'} /> : <Navigate to={'/menu'} /> } /> */}
-
-              {/* {logged ? (<Route path='/menuLogado' element={<HomeLog />} />) : (<Route path='/menu' element={<Home />} />)} */}
               <Route path='/' element={ logged ? <Navigate to={'/menu'} /> : <Navigate to={'/menuLogado'} /> } />
 
               <Route path='/menu' element={<Home />} />
@@ -64,20 +46,10 @@ const App = () => {
               <Route path='/login' element={<Login />} />
               <Route path='/cadastro' element={<Cadastro />} />
               <Route path='/novasenha' element={<Novasenha/>} />
-              <Route path='/loja/skins' element={<LojaSkin />} />
               <Route path='/loja/moedas' element={<LojaMoeda />} />
               <Route path='/conta' element={<Conta />} />
               <Route path='/tabuleiro' element={<Tabuleiro />} />
-              <Route path='/fila' element={<Fila />} />
-              <Route path='/erro' element={<Erro />} />
-              <Route path='/sucesso' element={<Sucesso />} />
               <Route path='/tutorial' element={<Tutorial />} />
-              <Route path='/vitoria/:id' element={<Vitoria />} />
-              <Route path='/derrota/:id' element={<Derrota />} />
-              <Route path='/desistencia/:id' element={<Desistencia/>} />
-              <Route path='/desistir' element={<Desistir />} />
-              <Route path='/compras/moedas/:id' element={<CompraMoedas />} />
-              <Route path='/compras/skins/:id' element={<CompraSkins />} />
               <Route path='/credito' element={<Credito />} />
             </Routes>
           </GlobalProvider>
--- src/hooks/SomAmbienteHook.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { SomAmbienteContext } from "../contexts/SomAmbienteContext";
-
-const useSomAmbiente = () => {
-    const context = useContext(SomAmbienteContext);
-    return context;
-};
-
-export default useSomAmbiente;
